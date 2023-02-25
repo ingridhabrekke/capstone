3d11a4148bd7f593a8ad5eb2087affbd41519e9c
Ingrid Håbrekke <ingri_dy0@Ingrids-MacBook-Air.local>
2023-02-25 13:12:06 +0800

    make modal for inputting projects

--- capstone-graveyard/src/components/Home/List/index.jsx
+++ capstone-graveyard/src/components/Home/List/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import { db } from "../../../firebase";
-
+import Modal from "react-overlays/Modal";
 
 function List() {
 	const [entries, setEntries] = useState([]);
@@ -22,18 +22,61 @@ function List() {
 		fetchData();
 	}, [])
 
+	
+	const [title, setTitle] = React.useState("")
+	const [text, setText] = React.useState("");
+	
+	// initialize college checklist (none checked)
+	const colleges = ["AH", "B", "CS", "NS", "SS"];
+	const [checkedState, setCheckedState] = useState(
+		new Array(colleges.length).fill(false)
+	);
+	// update list when checks change 
+	const handleChange = (position) => {
+		const updatedCheckedState = checkedState.map((item, index) =>
+			index === position ? !item : item
+		);
+		setCheckedState(updatedCheckedState);
+	};
+
+	// visibility of new entry modal
+	const [showModal, setShowModal] = useState(false);
+	var handleClose = () => setShowModal(false);
+
+
+	// add current "state" to database and close modal
+	const addToDb = async () => {
+		try {
+			const docRef = await addDoc(collection(db, "entries"), {
+				title: title,
+				text: text,
+				colleges: checkedState
+			});
+			setShowModal(false);
+			fetchData();
+			console.log("Document written with ID: ", docRef.id);
+			
+		} catch (e) {
+			console.error("Error adding document: ", e);
+		}
+	}
 
 	return (
 		<div className="">
-			<h1 className="p-8 pb-3 text-2xl">Projects (x results)</h1>
+			<span className="relative">
+				<h1 className="p-8 pb-3 text-2xl ">Projects (x results)</h1>
+				<button className="absolute top-0 right-8 bg-gray-300 p-2 rounded-md" onClick={() => setShowModal(true)}>
+					Add to Graveyard
+				</button>
+			</span>
 			<div className="container max-h-80 justify-start ">
 
 				{
 					entries?.map((entry, i) => (
-						<div key={i} className="mb-5 mt-5 block h-32 rounded-md border bg-white border-gray-300 m-8">
+						<div key={i} className="mb-5 mt-5 block h-32 shadow-md rounded-md border bg-white border-gray-300 m-8">
 							<div className="h-full grid-cols-5">
 								<div className="p-5">
-									<p>{entry.title}</p>
+									<h3>{entry.title}</h3>
 									<p>{entry.text}</p>
 								</div>
 							</div>
@@ -43,9 +86,68 @@ function List() {
 				}
 
 			</div>
+
+			<Modal
+				className="fixed w-1/2 h-1/2 z-50 top-1/4 left-1/4 border-1 rounded-md bg-white"
+				show={showModal}
+				onHide={handleClose}
+
+			>
+				<div>
+					<div className="fixed bg-black bg-opacity-80 inset-0 transition-opacity" id="addreview">
+						<div className="flex flex-col justify-center items-center">
+							<div className="m-20 p-10 bg-gray-200 rounded-md w-1/2 relative ">
+								<h2 className="text-2xl font-bold">Kill an Idea </h2>
+								<a className="absolute top-5 right-7 font-bold  hover:gray-800" onClick={handleClose}>&times;</a>
+								<div className="block mt-5">
+									<div>
+										<div className="space-y-5"  >
+											<div className="container">
+												<label className="container" htmlFor="title">Project Title: </label>
+												<input type="text" className="border-1 rounded-sm border-black" onChange={(e) => setTitle(e.target.value)}></input>
+											</div>
+
+											<div className="mt-4">
+												<label className="container" htmlFor="college">College(s):</label>
+												
+												{colleges.map((name, index) => {
+													return (
+														<div key={index} className="container inline pl-2">
+																	<input
+																		type="checkbox"
+																		id={`custom-checkbox-${index}`}
+																		name={name}
+																		value={name}
+																		checked={checkedState[index]}
+																		onChange={() => handleChange(index)}
+																	/>
+																	<label className="p-1" htmlFor={`custom-checkbox-${index}`}>{name}</label>
+														</div>
+													
+													);
+												})}
+											</div>							
+											<label className="container block" htmlFor="desc">Project Description</label>
+												<textarea className="container" placeholder="What is your project suggestion? Give us the deets" 
+												id="desc" name="desc" onChange={(e) => setText(e.target.value)}></textarea>
+											<button  className="bg-gray-500 p-2 block m-auto rounded-md" onClick={addToDb}>Bury idea</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+			</Modal>
 		</div>
+		
 
 	)
 };
 
 export default List; 
+
+
+
--- capstone-graveyard/src/pages/Home/index.jsx
+++ capstone-graveyard/src/pages/Home/index.jsx
@@ -2,41 +2,20 @@ import React from 'react';
 import NavBar from '../../components/Home/NavBar';
 import Filtering from '../../components/Home/Filtering';
 import List from '../../components/Home/List';
-import { db } from '../../firebase';
-import { collection, addDoc } from "firebase/firestore";
+
 
 
 function Home() {
-	const [text, setText] = React.useState("");
-	const [title, setTitle] = React.useState("")
-
-	const addToDb = async () => {
-		try {
-			const docRef = await addDoc(collection(db, "entries"), {
-				title: title,
-				text: text,
-			});
-			console.log("Document written with ID: ", docRef.id);
-		} catch (e) {
-			console.error("Error adding document: ", e);
-		}
-	}
 
 	return (
 		<div className="flex flex-col h-screen">
 			<NavBar />
 			<div className="flex flex-1 overflow-auto">
-				<div className="basis-64 overflow-auto shadow-2xl">
+				<div className="basis-72 overflow-auto shadow-2xl">
 					<Filtering />
 				</div>
 
 				<div className="flex-1 overflow-auto">
-					<div >
-						<input type="text" className="border-4 border-black" onChange={(e) => setTitle(e.target.value)}></input>
-						<input type="text" className="border-4 border-black" onChange={(e) => setText(e.target.value)}></input>
-						<button className="border-4 border-black" onClick={addToDb} >submit</button>
-					</div>
-
 					<List />
 				</div>
 			</div>
